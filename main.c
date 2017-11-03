39fe852e518211724a29f3f8416b4166d91b8a05
Theodore Ateba <tf.ateba@gmail.com>
2017-11-03 23:42:54 +0100

    Update the main file to use the new drivers files.

--- main.c
+++ main.c
@@ -1,164 +1,117 @@
-/*
- * @file     main.c 
- * @brief    Control a 4-Digit Display by software like I2C by using DIO
- * @author   Theodore ATEBA
- * @version  1.0
- * @date 04  July 2015
- * TODO:
- *      add a LDR to control the 4-Digit Display intensity
- *      Resolve the calendar.year problem
+/**
+ *
+ * @file     main.c
+ *
+ * @brief    Real Time Clock demo for DS1307 driver file.
+ *
+ * @author   Theodore Ateba, tf.ateba@gmail.com
+ *
+ * @date 21  June 2015
+ *
  */
 
-//=============================================================================
-// Include Files
-//=============================================================================
+/*==========================================================================*/
+/* Include Files.                                                           */
+/*==========================================================================*/
+
+/* Standard files. */
 #include <stdlib.h>
+
+/* CbibiOS files. */
 #include "ch.h"
 #include "hal.h"
-#include "dslib.h" // RTC Library
-#include "tmlib.h" // 4 Digit Display Library
-#include "chprintf.h"	// Library to send data over Serial
 
-BaseSequentialStream* pc = (BaseSequentialStream*) &SD2;
+/* Driver files. */
+#include "ds1307.h"
 
-//=============================================================================
-// Global variables, I2C TX and RX buffers, I2C and Serial Configurations
-//=============================================================================
+/*==========================================================================*/
+/* Variables, drivers configurations structures.                            */
+/*==========================================================================*/
 
-// Declaration of a structure of calendar
-struct ds1307_t calendar; // We will use the time for 4-digit Display
+//BaseSequentialStream* chp = (BaseSequentialStream*) &SD2;
 
-// Double Point state
-bool dp_flag = false;
+/* Declaration of a calendar. */
+rtcDriver_t calendar;
 
-// Data to print on the 4-Digit Display
-// 0 ==> minutes:secondes
-// 1 ==> hours:minutes
-// 2 ==> date
-// 3 ==> day
-// 4 ==> month
-// 5 ==> year
-uint8_t data2Print = 0;
+/*==========================================================================*/
+/* Functions.                                                               */
+/*==========================================================================*/
 
-//=============================================================================
-// Functions
-//=============================================================================
+/**
+ * @brief   RTC reader thread.
+ */
+static THD_WORKING_AREA(waRtcReadThread, 128);
+static THD_FUNCTION(RtcReadThread, arg) {
 
-/*
-* RTC reader thread
-*/
-static THD_WORKING_AREA (waRtcReadThread, 128);
-static THD_FUNCTION(RtcReadThread, arg){
-	(void)arg;
-	chRegSetThreadName ("RTC reader");
-	while (TRUE){
-		calendar = ds1307GetDate ();
-		chThdSleepMilliseconds (1000);
-	}
+  (void)arg;
+  chRegSetThreadName("RTC reader");
+
+  while (TRUE) {
+    ds1307GetClock(&calendar);
+    chThdSleepMilliseconds(800);
+  }
 }
 
-/*
-* RTC printer thread
-*/
-static THD_WORKING_AREA (waRtcPrintThread, 128);
-static THD_FUNCTION(RtcPrintThread, arg){
-	(void)arg;
-	chRegSetThreadName ("RTC printer");
-	while (TRUE){
-		ds1307Print(calendar);
-		dp_flag ^=1;
-		showTime(calendar, dp_flag, data2Print);
-		chThdSleepMilliseconds (1000);
-	}
+/**
+ * @brief   RTC printer thread.
+ */
+static THD_WORKING_AREA(waRtcPrintThread, 128);
+static THD_FUNCTION(RtcPrintThread, arg) {
+  (void)arg;
+  chRegSetThreadName("RTC printer");
+
+  while (TRUE) {
+    ds1307PrintClock(&calendar);
+    chThdSleepMilliseconds(1000);
+  }
 }
 
-/*
-* Trigger the User Button
-*/
-static void extcb(EXTDriver *extp, expchannel_t channel){
-	(void)extp;
-	(void)channel;
+/**
+ * @brief   Application entry point.
+ */
+int main (void) {
 
-	data2Print++;
-	if(data2Print == 6)
-		data2Print = 0;
+  /* System initializations. */
+  halInit();
+  chSysInit();
+
+  /* Init the I2C interface 1. */
+  ds1307InitInterface();
+
+  /* Init the serial Driver. */
+  sdStart(&SD2, NULL);
+  print("\n\r Chibios Real Time Clock calendar with Nucleo and DS1307.\n\r");
+
+  /* Used when you whant to set the calendar and clock. */
+  /* TODO: Try to use the compilation time of the program to update the RTC. */
+  /* TODO: Use the shell to update the RTC. */
+  /* TODO: Use the shell to read the RTC. */
+
+  /* Configure the RTC parameters. */
+  calendar.rtc.seconds  = 0;
+  calendar.rtc.minutes  = 32;
+  calendar.rtc.hours    = 23;
+  calendar.rtc.day      = 3;
+  calendar.rtc.date     = 24;
+  calendar.rtc.month    = 6;
+  calendar.rtc.year     = 2015;
+  calendar.refYear      = 2000;
+
+  /* TODO: Add shell to read RTC data. */
+  /* TODO: Use the Shell to set the RTC parameter. */
+
+  //ds1307SetClock(&calendar);
+
+  /* Create the thread used to read the RTC DS1307. */
+  chThdCreateStatic(waRtcReadThread, sizeof(waRtcReadThread), NORMALPRIO,
+                    RtcReadThread, NULL);
+
+  /* Create the thread used to print the RTC data every seconds. */
+  chThdCreateStatic(waRtcPrintThread, sizeof(waRtcPrintThread), NORMALPRIO,
+                    RtcPrintThread, NULL);
+
+  while (TRUE) {
+    chThdSleepMilliseconds (1);
+  }
 }
 
-static const EXTConfig extConfig = {
-	{
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_RISING_EDGE | EXT_CH_MODE_AUTOSTART | EXT_MODE_GPIOC, extcb},
-	    {EXT_CH_MODE_DISABLED, NULL},
-		{EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	    {EXT_CH_MODE_DISABLED, NULL},
-	}
-};
-
-/*
-* @fn       main
-* @brief    Application entry point.
-*/
-int main (void){
-	// System initializations.
-	halInit();
-	chSysInit();
-
-	// Init the I2C interface 1
-	ds1307InterfaceInit();
-        
-	// init the serial Driver 2 to print at the same time the date.
-	sdStart(&SD2, NULL);
-    
-	// Set the GPIOs pin used
-	palSetPadMode(GPIOC, DIO_PIN, PAL_MODE_INPUT_PULLUP);
-	palClearPad(GPIOC, DIO_PIN);
-	palSetPadMode(GPIOC, CLK_PIN, PAL_MODE_INPUT_PULLUP);
-	palClearPad(GPIOC, CLK_PIN);
-
-	// Activates the Button Interruption
-	extStart(&EXTD1, &extConfig);
-	chprintf(pc, "\n\r Chibios Real Time Clock calendar with Nucleo and DS1307 printing also on 4Digit Display.\n\r");
-    
-	// Used when you whant to set the calendar and clock
-	calendar.seconds    = 0;
-	calendar.minutes    = 54;
-	calendar.hours      = 11; 
-	calendar.day        = 2;
-	calendar.date       = 6;
-	calendar.month      = 10;
-	calendar.year       = 2015;
-	//ds1307SetDate(calendar);
-
-	// Create the thread used to read the RTC DS1307
-	chThdCreateStatic (waRtcReadThread, sizeof(waRtcReadThread), NORMALPRIO+1, 
-	RtcReadThread, NULL);
-    
-	// Create the thread used to print the RTC data every seconds
-	chThdCreateStatic (waRtcPrintThread, sizeof(waRtcPrintThread), NORMALPRIO, 
-	RtcPrintThread, NULL);
-  
-	// Set the display intensity before sending data to print.
-	setBrightness (0x0F);
-
-	while (1){
-		chThdSleepMilliseconds (500);
-	}
-}
